9f81416b2e6b87498543ed8a94358f6158c3a308
Amps <amps2970@gmail.com>
2025-03-29 15:54:33 +0530

    Tabs

--- Native/app/(tabs)/index.tsx
+++ Native/app/(tabs)/index.tsx
@@ -5,9 +5,8 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary font-bold">Hi family.</Text>
-      <Link href="/Onboarding">Go to Onboarding</Link> 
+      
 
     </View>
   );
 }
-

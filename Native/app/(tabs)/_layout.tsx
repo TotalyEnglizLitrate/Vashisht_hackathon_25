98e92f8e95dd40e2c6b6da81a1bafe73e6836444
Amps <amps2970@gmail.com>
2025-03-29 15:54:33 +0530

    Tabs

---
+++ Native/app/(tabs)/_layout.tsx
@@ -0,0 +1,95 @@
+import { Tabs } from "expo-router";
+import { ImageBackground, Image, Text, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+
+function TabIcon({ focused, icon, title }: any) {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+}
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
+      {/* Home Screen */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+
+      {/* Items Screen */}
+      <Tabs.Screen
+        name="items"
+        options={{
+          title: "Items",
+          headerShown: false,
+          tabBarLabel: ({ focused }) => (
+            <Text className={focused ? "text-white font-bold" : "text-gray-400"}>
+              Profile
+            </Text>
+          ),
+        }}
+      />
+
+      {/* User/ Profile Screen */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarLabel: ({ focused }) => (
+            <Text className={focused ? "text-white font-bold" : "text-gray-400"}>
+              Items
+            </Text>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
